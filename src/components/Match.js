a83aa2b96368f4131948b876e47c4e8762b33ba0
Son Hyejoo <107318155+sonhyejoo@users.noreply.github.com>
2023-04-01 14:48:49 -0400

    match button state done

---
+++ src/components/Match.js
@@ -0,0 +1,24 @@
+import { useState, useContext } from "react";
+import { HoroscopeContext } from "../context/HoroscopeContext";
+
+const Match = () => {
+  const [match, setMatch] = useState(false);
+  const { sign } = useContext(HoroscopeContext);
+  console.log(sign);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setMatch(!match);
+        }}
+      >
+        {" "}
+        show Match
+      </button>
+      {match && <div>{sign.match}</div>}
+    </>
+  );
+};
+
+export default Match;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,24 @@
-import horoscopesObj from '../data/horoscopes';
+import horoscopesObj from "../data/horoscopes";
+import { useContext } from "react";
+import { HoroscopeContext } from "../context/HoroscopeContext";
 
 const Navbar = () => {
   const horoscopes = Object.keys(horoscopesObj);
-
+  const { setCurrentSign } = useContext(HoroscopeContext);
   return (
     <nav>
-      {horoscopes.map(sign => (
-        <span key={sign}>
-        {sign}
+      {horoscopes.map((sign) => (
+        <span
+          onClick={() => {
+            setCurrentSign(sign);
+          }}
+          key={sign}
+        >
+          {sign}
         </span>
       ))}
     </nav>
-  )
+  );
 };
 
 export default Navbar;
